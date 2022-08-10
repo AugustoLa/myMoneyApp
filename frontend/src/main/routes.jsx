aab58ddb8057806353214f989fa584115427eb4f
Augusto <augusto_florindo@hotmail.com>
2022-08-10 00:56:55 -0300

    first commit

---
+++ frontend/src/main/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Router, Route, Redirect, hashHistory} from 'react-router'
+import BillingCycle from '../billingCycle/billingCycle'
+import Dashboard from '../dashboard/dashboard'
+
+export default props => (
+    <Router history={hashHistory}>
+        <Route path='/' component={Dashboard}></Route>
+        <Route path='/billingCycles' component={BillingCycle}></Route>
+        <Redirect from='*' to='/'></Redirect>
+    </Router>
+)
